368f0d00b931141f6097e552c73b6e89c72547ee
João Padilha <jvlpadilha@outlook.com>
2022-05-16 16:42:38 -0300

    redo-fetchAnimais

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from './modules/modal.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import Tooltip from './modules/tooltip.js';
@@ -25,10 +25,16 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init()
 
+
+
+fetchAnimais('../../animaisapi.json','.numeros-grid');
+
+
+
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
 initAnimacaoScroll();
 
